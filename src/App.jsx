73ae9bce5d3e8882214a958af9b1f53005a849c1
kilavkiwork <kilavki@outlook.com>
2025-11-16 00:12:23 +0200

    feat: currency receiving functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,23 @@ import "./index.css";
 const API_URL = "https://api.frankfurter.dev/v1";
 
 function App() {
+  const [currencies, setCurrencies] = useState([]);
+  const [fromCurrency, setFromCurrency] = useState("EUR");
+  const [toCurrency, setToCurrency] = useState("USD");
+  const [amount, setAmount] = useState(1);
+
+  useEffect(() => {
+    async function getCurrencies() {
+      const rsp = await fetch(`${API_URL}/latest`);
+      const data = await rsp.json();
+      // console.log(data);
+      setCurrencies(Object.keys(data.rates));
+    }
+    getCurrencies();
+  }, []);
+
+  console.log(currencies);
+
   return (
     <div className="app">
       <h1>Currency Exchange Calculator</h1>
@@ -12,13 +29,35 @@ function App() {
         <p className="error"></p>
 
         <div className="input-group">
-          <input type="number" placeholder="Amount" className="input-field" />
-          <select className="dropdown">
-            <option></option>
+          <input
+            type="number"
+            placeholder="Amount"
+            className="input-field"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+
+          <select
+            className="dropdown"
+            onChange={(e) => setFromCurrency(e.target.value)}
+          >
+            {currencies.map((item) => (
+              <option key={item} value={item}>
+                {item}
+              </option>
+            ))}
           </select>
           <span className="arrow">→</span>
-          <select className="dropdown">
-            <option></option>
+
+          <select
+            className="dropdown"
+            onChange={(e) => setToCurrency(e.target.value)}
+          >
+            {currencies.map((item) => (
+              <option key={item} value={item}>
+                {item}
+              </option>
+            ))}
           </select>
         </div>
         <button className="convert-button">Convert</button>
